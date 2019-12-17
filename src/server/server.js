84a5890835d3c58dd761650e724f7dab8bf938b9
Robert Baker <zanotam@gmail.com>
2019-12-16 22:16:09 -0700

    Omniproxy boooy

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/model');
+const cors = require('cors');
+
 const server = express();
+
+server.use(cors());
+
 
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.json()); //sets up req.body without needing additional middleware
